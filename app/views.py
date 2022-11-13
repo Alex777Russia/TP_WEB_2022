8308186656b69d75baf2ccbe63feef94d442c898
Alex777Russia <shevalex777@mail.ru>
2022-11-13 12:10:48 +0300

    views skeleton added

--- app/views.py
+++ app/views.py
@@ -5,3 +5,21 @@ from django.views.decorators.http import require_GET
 
 def index(request):
     return render(request, 'index.html')
+
+def question(request, question_id: int):
+    return render(request, 'question.html')
+
+def new_question(request):
+    return render(request, 'new_question.html')
+
+def tag_question(request):
+    return render(request, 'tag_question.html')
+
+def settings(request):
+    return render(request, 'settings.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def signup(request):
+    return render(request, 'signup.html')
--- askme/urls.py
+++ askme/urls.py
@@ -19,5 +19,11 @@ from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name = 'index')
+    path('', views.index, name = 'index'),
+    path('question</int:question_id>', views.question, name = 'question'),
+    path('new_question', views.new_question, name = 'new_question'),
+    path('tag_question', views.tag_question, name = 'tag_question'),
+    path('settings', views.settings, name = 'settings'),
+    path('login', views.login, name = 'login'),
+    path('signup', views.signup, name = 'signup'),
 ]
