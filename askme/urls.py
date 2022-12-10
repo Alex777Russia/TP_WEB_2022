66542ac94e37b4fe3aadd70008ddffec5737e059
Alex777Russia <shevalex777@mail.ru>
2022-12-10 20:51:53 +0300

    added models | chenged urls, views, settings | added generate_database

--- askme/urls.py
+++ askme/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,4 +30,4 @@ urlpatterns = [
     path('settings/', views.settings, name='settings'),
     path('tag/<str:tag>', views.tag_questions, name='tag_questions'),
     path('', views.new_questions, name='root'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
